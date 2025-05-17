ab9dfcb9b615a56536084c7db79113b5f027e949
kinamy200111 <vladik.dvornikov@inbox.ru>
2025-05-17 21:18:33 +0300

    Update CreatorLobby.lua

--- CreatorLobby.lua
+++ CreatorLobby.lua
@@ -1,17 +1,25 @@
 local Event = game:GetService("ReplicatedStorage").MainHandler
+
+-- Создание комнаты
 Event:FireServer(table.unpack({
-    {
-        "CreateRoom",
-        "",
-        "15452"
-    }
+    "CreateRoom",
+    "",
+    "15452"
 }))
-wait(0.5)
-local Event = game:GetService("ReplicatedStorage").MainHandler
+
+task.wait(0.5)  -- Лучше использовать task.wait вместо wait
+
+-- Запуск игры
 Event:FireServer(table.unpack({
-    {
-        "Start",
-        "",
-    }
+    "Start",
+    ""
 }))
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kinamy200111/BlockadeScript/main/Farm.lua"))()
+
+-- Загрузка скрипта для фарма с обработкой ошибок
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinamy200111/BlockadeScript/main/Farm.lua"))()
+end)
+
+if not success then
+    warn("Ошибка загрузки Farm.lua: " .. tostring(err))
+end
