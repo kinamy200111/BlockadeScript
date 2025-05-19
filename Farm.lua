45d74d5e2517aa8e68f357bcc64e7521e2a66bd3
kinamy200111 <vladik.dvornikov@inbox.ru>
2025-05-19 08:01:23 +0300

    Update Farm.lua

--- Farm.lua
+++ Farm.lua
@@ -7,9 +7,9 @@ local VirtualInput = game:GetService("VirtualInputManager")
 -- Конфигурация
 local CONFIG = {
     -- Настройки фарма
-    FARM_HEIGHT = 50,
-    FARM_RADIUS = 25,
-    FARM_SPEED = 1.00,
+    FARM_HEIGHT = 60,
+    FARM_RADIUS = 23,
+    FARM_SPEED = 0.09,
     ATTACK_DELAY = 0.5,
     HEAL_TRESHOLD = 700,
     HEAL_COOLDOWN = 0.2,
